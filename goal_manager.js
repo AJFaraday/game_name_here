6dc961fb447dc9d1b781dbb4aee8fae94b872721
andrew james faraday <ajfaraday@batman.alces.co.uk>
2018-06-29 15:39:06 +0100

    fixed dual-spawn issue

--- goal_manager.js
+++ goal_manager.js
@@ -11,5 +11,8 @@ GoalManager = function(rows, cols) {
 
   this.add_goal = function(x, y) {
     Sketch.rows[y][x].is_goal();
+    if(typeof API != 'undefined' && API.goal[0] == API.coord[0] && API.goal[1] == API.coord[1]) {
+      API.current_cell().visit();
+    }
   };
 };
